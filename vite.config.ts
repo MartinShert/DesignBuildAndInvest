830bbd83b6582924fd0887904cc1df6790042e3c
Nick Turner <61646469+MartinShert@users.noreply.github.com>
2025-06-07 14:13:14 +0000

    Build ready for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,17 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  server: {
-    allowedHosts: true,
-  },
   plugins: [react()],
+  server: {
+    port: 5173,
+    strictPort: true,
+    allowedHosts: ["tmqshl-5173.csb.app", "tmqshl-5175.csb.app"], // <-- both hosts in the array
+  },
+  preview: {
+    port: 4173,
+    strictPort: true,
+  },
+  build: {
+    outDir: "dist",
+  },
 });
